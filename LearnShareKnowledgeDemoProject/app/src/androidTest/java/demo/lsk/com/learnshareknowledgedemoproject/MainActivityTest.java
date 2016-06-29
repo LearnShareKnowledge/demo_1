115bc6480f18b1d0761b17515c7ebd133dc207de
310124463 <anukool.tiwari@philips.com>
2016-06-29 17:02:43 +0200

    Added test case

---
+++ LearnShareKnowledgeDemoProject/app/src/androidTest/java/demo/lsk/com/learnshareknowledgedemoproject/MainActivityTest.java
@@ -0,0 +1,32 @@
+package demo.lsk.com.learnshareknowledgedemoproject;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by 310124463 on 6/29/2016.
+ */
+public class MainActivityTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+        assertTrue(true);
+
+    }
+
+    public void testOnCreateOptionsMenu() throws Exception {
+
+    }
+
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+}
